f7016d092bddff8ed17a8459cda8836e70e46184
Simon La Grange <sla-gran@shi-r4-p5.s19.be>
2025-10-16 10:43:50 +0200

    first commit

---
+++ ft_memmove.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sla-gran <sla-gran@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 16:32:32 by sla-gran          #+#    #+#             */
+/*   Updated: 2025/10/15 16:32:32 by sla-gran         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	unsigned char *d;
+	unsigned const char *s;
+
+	i = 0;
+	d = dest;
+	s = src;
+}
---
+++ ft_memset.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sla-gran <sla-gran@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 13:10:24 by sla-gran          #+#    #+#             */
+/*   Updated: 2025/10/14 13:10:24 by sla-gran         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+void	*ft_memset(void *str, int c, size_t n)
+{
+	size_t	i;
+	unsigned char *s;
+
+	s = str;
+	i = 0;
+	while (i < n)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (str);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	char s[6] = "test12";
+	ft_memset(s, 'd', 1);
+	printf("%s \n", s);
+}

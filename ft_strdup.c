c0f3f0f86b8657a1dfaba6f0315adc6a25b32f53
Simon La Grange <sla-gran@fu-r3-p4.s19.be>
2025-10-20 15:59:55 +0200

    strdup

---
+++ ft_strdup.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sla-gran <sla-gran@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/20 15:53:46 by sla-gran          #+#    #+#             */
+/*   Updated: 2025/10/20 15:53:46 by sla-gran         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+size_t	ft_strlen(const char *s);
+
+char	*ft_strdup(const char *s)
+{
+	char	*dest;
+	int		s_len;
+	int		i;
+
+	i = 0;
+	s_len = ft_strlen(s);
+	dest = malloc((s_len + 1) * sizeof(char));
+	if (!dest)
+		return (NULL);
+	while (s[i])
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+
--- libft.h
+++ libft.h
@@ -13,7 +13,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stddef.h>
+# include <stdlib.h>
 
 typedef struct		s_list
 {

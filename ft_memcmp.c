da6e0ec156fea4bcacab2386bd1a4f0bef16d2c3
Simon La Grange <sla-gran@fu-r3-p4.s19.be>
2025-10-20 15:51:53 +0200

    3e colone done

---
+++ ft_memcmp.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sla-gran <sla-gran@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/20 15:18:03 by sla-gran          #+#    #+#             */
+/*   Updated: 2025/10/20 15:18:03 by sla-gran         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+int	memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	i = 0;
+	str1 = s1;
+	str2 = s2;
+	while (i <= n && str1[i] == str2[i])
+		i++;
+	return (str1[i] - str2[i]);
+}
